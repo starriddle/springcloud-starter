05ce9c2daad2ec38a5699d40ac7a69d4b85b9851
yunliang.chen <yunliang.chen@iqunxing.com>
2018-01-28 20:21:56 +0800

    add framework modules series

---
+++ framework/chapter1/src/main/java/com/cyl/demo/framework/chapter1/HelloServlet.java
@@ -0,0 +1,32 @@
+package com.cyl.demo.framework.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 接受 GET 类型请求 /hello，
+ * 转发到 /WEB-INF/jsp/hello.jsp 页面，
+ * 页面显示系统当前时间。
+ *
+ * @author CYL
+ * @version 1.0
+ * @date 2018-01-28
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String now = sdf.format(new Date());
+        req.setAttribute("now", now);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+
+}
---
+++ framework/chapter1/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: starr
+  Date: 2018/01/28
+  Time: 19:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+
+<h1>Hello!</h1>
+
+<h2>当前时间：${now}</h2>
+
+</body>
+</html>
