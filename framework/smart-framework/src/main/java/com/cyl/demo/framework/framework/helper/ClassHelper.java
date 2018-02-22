d985f7f3001ecf8d416f60b3a95fd9c3156bee34
starriddle <chen.86@outlook.com>
2018-02-22 18:29:18 +0800

    demo.framework: add chapter3 & framework

---
+++ framework/smart-framework/src/main/java/com/cyl/demo/framework/framework/helper/ClassHelper.java
@@ -0,0 +1,81 @@
+package com.cyl.demo.framework.framework.helper;
+
+import com.cyl.demo.framework.framework.annotation.Controller;
+import com.cyl.demo.framework.framework.annotation.Service;
+import com.cyl.demo.framework.framework.util.ClassUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 类操作助手类
+ *
+ * @author CYL
+ * @date 2018-02-22
+ */
+public final class ClassHelper {
+
+    /**
+     * 定义类集合（用于存放所加载的类）
+     */
+    private static final Set<Class<?>> CLASS_SET;
+
+    static {
+        String basePackage = ConfigHelper.getAppBasePackage();
+        CLASS_SET = ClassUtil.getClassSet(basePackage);
+    }
+
+    /**
+     * 获取应用包名下的所有类
+     *
+     * @return
+     */
+    public static Set<Class<?>> getClassSet(){
+        return CLASS_SET;
+    }
+
+    /**
+     * 获取应用包名下的所有 Service 类
+     *
+     * @return
+     */
+    public static Set<Class<?>> getServiceClassSet(){
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET){
+            if (cls.isAnnotationPresent(Service.class)){
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 获取应用包名下的所有 Controller 类
+     *
+     * @return
+     */
+    public static Set<Class<?>> getControllerClassSet(){
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET){
+            if (cls.isAnnotationPresent(Controller.class)){
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 获取应用包名下的所有 Bean 类（包括：Service、Controller等）
+     *
+     * @return
+     */
+    public static Set<Class<?>> getBeanClassSet(){
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        classSet.addAll(getControllerClassSet());
+        classSet.addAll(getServiceClassSet());
+        return classSet;
+    }
+
+
+
+}
---
+++ framework/smart-framework/src/main/java/com/cyl/demo/framework/framework/util/ReflectionUtil.java
@@ -0,0 +1,16 @@
+package com.cyl.demo.framework.framework.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 反射工具类
+ * 封装Java反射相关的API，对外提供更好用的工具方法
+ *
+ * @author CYL
+ * @date 2018-02-22
+ */
+public class ReflectionUtil {
+
+    private  static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtil.class);
+}
