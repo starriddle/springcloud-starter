897d3b0c563665974e93c9add38cb7076ac6398c
starriddle <chen.86@outlook.com>
2018-02-22 22:21:42 +0800

    update framework: 3.6+3.7

---
+++ framework/smart-framework/src/main/java/com/cyl/demo/framework/framework/helper/IocHelper.java
@@ -0,0 +1,49 @@
+package com.cyl.demo.framework.framework.helper;
+
+import com.cyl.demo.framework.framework.annotation.Inject;
+import com.cyl.demo.framework.framework.util.ArrayUtil;
+import com.cyl.demo.framework.framework.util.CollectionUtil;
+import com.cyl.demo.framework.framework.util.ReflectionUtil;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+/**
+ * 依赖注入助手类
+ *
+ * @author CYL
+ * @date 2018-02-22
+ */
+public class IocHelper {
+
+    static {
+        // 获取所有的Bean类与Bean实例之间的映射关系（简称 Bean Map）
+        Map<Class<?>, Object> beanMap = BeanHelper.getBeanMap();
+        if (CollectionUtil.isNotEmpty(beanMap)) {
+            //遍历 Bean Map
+            for (Map.Entry<Class<?>, Object> beanEntry : beanMap.entrySet()) {
+                // 从 BeanMap 中获取 Bean 类与 Bean 实例
+                Class<?> beanClass = beanEntry.getKey();
+                Object beanInstance = beanEntry.getValue();
+                // 获取 Bean 类定义的所有成员变量（简称 Bean Field）
+                Field[] beanFields = beanClass.getDeclaredFields();
+                if (ArrayUtil.isNotEmpty(beanFields)){
+                    // 遍历 Bean Field
+                    for (Field beanField : beanFields) {
+                        // 判断当前 Bean Field 是否带有 Inject 注解
+                        if (beanField.isAnnotationPresent(Inject.class)) {
+                            // 在 Bean Map 中获取 Bean Field 对应的实例
+                            Class<?> beanFieldClass = beanField.getType();
+                            Object beanFieldInstance = beanMap.get(beanFieldClass);
+                            if (beanFieldInstance != null) {
+                                // 通过反射初始化 Bean Field 的值
+                                ReflectionUtil.setField(beanInstance, beanField, beanFieldInstance);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
--- framework/smart-framework/src/main/java/com/cyl/demo/framework/framework/util/ReflectionUtil.java
+++ framework/smart-framework/src/main/java/com/cyl/demo/framework/framework/util/ReflectionUtil.java
@@ -3,6 +3,9 @@ package com.cyl.demo.framework.framework.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 /**
  * 反射工具类
  * 封装Java反射相关的API，对外提供更好用的工具方法
@@ -13,4 +16,51 @@ import org.slf4j.LoggerFactory;
 public class ReflectionUtil {
 
     private  static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtil.class);
+
+    /**
+     * 根据类创建实例
+     *
+     * @param cls 需要创建实例的类
+     * @return 实例
+     */
+    public static Object newInstance(Class<?> cls) {
+        Object instance;
+        try {
+            instance = cls.newInstance();
+        } catch (ReflectiveOperationException e) {
+            LOGGER.error("new instance failure", e);
+            throw new RuntimeException(e);
+        }
+        return instance;
+    }
+
+    /**
+     * 调用方法
+     *
+     * @param object 类的对象
+     * @param method 类的实例方法
+     * @param args 方法的参数
+     * @return
+     */
+    public static Object invokeMethod(Object object, Method method, Object... args) {
+        Object result;
+        method.setAccessible(true);
+        try {
+            result = method.invoke(object, args);
+        } catch (ReflectiveOperationException e) {
+            LOGGER.error("invoke method failure", e);
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    public static void setField(Object object, Field field, Object value) {
+        field.setAccessible(true);
+        try {
+            field.set(object, value);
+        } catch (IllegalAccessException e) {
+            LOGGER.error("set field failure", e);
+            throw new RuntimeException(e);
+        }
+    }
 }
