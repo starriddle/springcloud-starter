d3ecb66617757df48ff288c7350a5a79df6c7c3f
starriddle <chen.86@outlook.com>
2019-01-12 16:13:20 +0800

    config eureka 修改启动类

--- config/config-eureka/src/main/java/com/starriddle/starter/springcloud/config/eureka/ConfigEurekaApplication.java
+++ config/config-eureka/src/main/java/com/starriddle/starter/springcloud/config/eureka/ConfigEurekaApplication.java
@@ -12,10 +12,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class ConfigEurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(ConfigEurekaApplication.class, args);
     }
 
 }
